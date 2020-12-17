bf16376c444a93ca4fcb901b28316214c206b92c
Yisi Liu <yisiliu@gmail.com>
2020-12-17 19:21:45 +0800

    Remove the unused code

--- test/TestTP.js
+++ test/TestTP.js
@@ -12,12 +12,6 @@ var pool_id;
 var _total_tokens;
 var _limit;
 
-const fromHexString = hexString =>
-  new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-
-const toHexString = bytes =>
-  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
-
 contract("HappyTokenPool", accounts => {
     beforeEach(async () =>{
         console.log("Before ALL\n");
